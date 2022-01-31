e6fb9bb55cee95b2357bf11718e5dc421c68457c
theyanaga <felipe@yanaga.com.br>
2022-01-31 13:13:23 -0500

    working react

---
+++ src/main/webapp/my-app/src/Components/Table.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  
