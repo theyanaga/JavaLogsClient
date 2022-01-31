c0b5ef76fb36a09d8f1965fa9bc5e1ab5355e1e7
theyanaga <felipe@yanaga.com.br>
2022-01-31 13:46:39 -0500

    added table and fixed proxy

--- src/main/webapp/my-app/src/App.tsx
+++ src/main/webapp/my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import BasicTable from './Components/Table';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+      BasicTable()
+      )
+      /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */
 }
 
 export default App;
