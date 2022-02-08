77a5637fd0886b1d10b09546f3841d52deabcda7
yanaga <edson@yanaga.io>
2022-02-08 15:52:24 -0500

    Added a Repository to return a Map<User, List<String>> from RowFromServer

--- src/main/java/api/calls/helpers/AResource.java
+++ src/main/java/api/calls/helpers/AResource.java
@@ -1,6 +1,16 @@
 package api.calls.helpers;
 
-import api.calls.entities.Log;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import gradingTools.logs.localChecksStatistics.collectors.Collector;
 import gradingTools.logs.localChecksStatistics.collectors.StandardCollectors.AttemptsCollectorV2;
 import gradingTools.logs.localChecksStatistics.collectors.StandardCollectors.FinalStatusCollector;
@@ -8,21 +18,16 @@ import gradingTools.logs.localChecksStatistics.compiledLogGenerator.CollectorMan
 import gradingTools.logs.localChecksStatistics.compiledLogGenerator.LocalLogDataAnalyzer;
 import parsing.entities.LocalChecksTest;
 import parsing.entities.RowFromServer;
+import parsing.entities.RowFromServerRepository;
 import parsing.entities.User;
 import parsing.entities.UserWithTests;
 import parsing.helpers.AndrewOutputProcessor;
 
-import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Path("/potato")
 public class AResource {
+
+    @Inject
+    RowFromServerRepository rowFromServerRepository;
 
     @GET
     @Transactional
@@ -78,33 +83,16 @@ public class AResource {
 
     @Path("/allUsers")
     @GET
-    @Transactional(Transactional.TxType.REQUIRES_NEW)
     @Produces(MediaType.APPLICATION_JSON)
     public List<UserWithTests> getAllUsersAndTheirTests() {
-        List<User> users = User.listAll();
-        List<UserWithTests> userWithTests = new ArrayList<>();
-
-        for (User user : users) {
-            List<RowFromServer> rowsForUser = RowFromServer.find("user_id", user.id).list();
-
-            if (rowsForUser.size() > 0) {
-                Collector[] collectors = {
-                        new AttemptsCollectorV2(),
-                        new FinalStatusCollector(),
-                };
-                CollectorManager cm = new CollectorManager(collectors);
-
-                List<String> lines = new ArrayList<>();
-
-                for (RowFromServer row : rowsForUser) {
-                    lines.add(row.createCSVLineFromRow());
-                }
-                userWithTests.add(UserWithTests.of(user , AndrewOutputProcessor.processInput(LocalLogDataAnalyzer.runEvaluationFromDatabase(lines, cm))));
-            }
-
-        }
-
-        return userWithTests;
+        return rowFromServerRepository.findCsvLineByUserMap().entrySet().stream()
+            .map(entry -> {
+                CollectorManager cm = new CollectorManager(new Collector[] {new AttemptsCollectorV2(), new FinalStatusCollector()});
+                return UserWithTests.of(
+                    entry.getKey(), 
+                    AndrewOutputProcessor.processInput(LocalLogDataAnalyzer.runEvaluationFromDatabase(entry.getValue(), cm)));
+            })
+            .collect(Collectors.toList());
     }
 
 }
